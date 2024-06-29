24ba26167aa6332484036f221f768e62e251913a
krishna.patel <krishna.patel@emgage.work>
2024-06-29 10:06:18 +0530

    initial commit

---
+++ ExamPaperDistributionSystem/ExamPaperDistributionSystem/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ExamPaperDistributionSystem.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ExamPaperDistributionSystem/ExamPaperDistributionSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExamPaperDistributionSystem.Startup))]
+namespace ExamPaperDistributionSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
